9f74266fd0e87a027666f250a227c870af90100b
AlexNicolici <alexandru_nicolici@yahoo.com>
2023-04-07 14:32:11 +0300

    refactoring movies list into component

---
+++ src/pages/components/movies/MoviesListComponent.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+
+function MoviesListComponent({
+  filterByRelease,
+  moviesList,
+  globalSearchList,
+  sortedOrderAscending,
+  sortedKey,
+  setMoviesList,
+}) {
+  const handleDeleteMovie = (e, item) => {
+    e.preventDefault();
+
+    const deleteMovie = sortedList.filter((movie) => {
+      if (movie.id !== item.id) {
+        return true;
+      }
+    });
+    setMoviesList(deleteMovie);
+  };
+
+  const filteredMoviesByAge = moviesList.filter((movie) => {
+    if (!filterByRelease.ageStart) {
+      return true;
+    }
+    if (
+      movie.release >= filterByRelease.ageStart &&
+      movie.release <= filterByRelease.ageEnd
+    ) {
+      return true;
+    }
+  });
+
+  const searchedMoviesList = filteredMoviesByAge.filter((movie) => {
+    return (
+      movie.name.toLowerCase().includes(globalSearchList.toLowerCase()) ||
+      movie.author.toLowerCase().includes(globalSearchList.toLowerCase())
+    );
+  });
+
+  const sortedList = [...searchedMoviesList].sort((a, b) => {
+    if (sortedOrderAscending) {
+      if (a[sortedKey] < b[sortedKey]) {
+        return -1;
+      }
+      if (a[sortedKey] > b[sortedKey]) {
+        return 1;
+      }
+      return 0;
+    } else {
+      if (a[sortedKey] < b[sortedKey]) {
+        return 1;
+      }
+      if (a[sortedKey] > b[sortedKey]) {
+        return -1;
+      }
+      return 0;
+    }
+  });
+
+  return (
+    <div className="movies-list">
+      <ul>
+        {sortedList.map((item, idx) => {
+          return (
+            <li key={`${idx}-${item}`}>
+              <p>Name: {item.name}</p>
+              <p>Release: {item.release}</p>
+              <p>Author: {item.author}</p>
+              <p>Type: {item.type}</p>
+              <button
+                style={{ marginBottom: 5 }}
+                onClick={(e) => handleDeleteMovie(e, item)}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default MoviesListComponent;
--- src/pages/movies.tsx
+++ src/pages/movies.tsx
@@ -3,6 +3,7 @@ import FilterComponent from "./components/movies/FilterComponent";
 import SortingComponent from "./components/movies/SortingComponent";
 import AddMovieComponent from "./components/movies/AddMovieComponent";
 import GlobalSearchComponent from "./components/movies/GlobalSearchComponent";
+import MoviesListComponent from "./components/movies/MoviesListComponent";
 
 export const movies = [
   {
@@ -83,56 +84,6 @@ function Movies() {
   const [sortedKey, setSortedKey] = useState("release");
   const [sortedOrderAscending, setSortedOrderAscending] = useState(true);
 
-  const handleDeleteMovie = (e, item) => {
-    e.preventDefault();
-
-    const deleteMovie = sortedList.filter((movie) => {
-      if (movie.id !== item.id) {
-        return true;
-      }
-    });
-    setMoviesList(deleteMovie);
-  };
-
-  const filteredMoviesByAge = moviesList.filter((movie) => {
-    if (!filterByRelease.ageStart) {
-      return true;
-    }
-    if (
-      movie.release >= filterByRelease.ageStart &&
-      movie.release <= filterByRelease.ageEnd
-    ) {
-      return true;
-    }
-  });
-
-  const searchedMoviesList = filteredMoviesByAge.filter((movie) => {
-    return (
-      movie.name.toLowerCase().includes(globalSearchList.toLowerCase()) ||
-      movie.author.toLowerCase().includes(globalSearchList.toLowerCase())
-    );
-  });
-
-  const sortedList = [...searchedMoviesList].sort((a, b) => {
-    if (sortedOrderAscending) {
-      if (a[sortedKey] < b[sortedKey]) {
-        return -1;
-      }
-      if (a[sortedKey] > b[sortedKey]) {
-        return 1;
-      }
-      return 0;
-    } else {
-      if (a[sortedKey] < b[sortedKey]) {
-        return 1;
-      }
-      if (a[sortedKey] > b[sortedKey]) {
-        return -1;
-      }
-      return 0;
-    }
-  });
-
   return (
     <div className="movies-page">
       <GlobalSearchComponent
@@ -159,26 +110,14 @@ function Movies() {
         setMoviesList={setMoviesList}
       />
 
-      <div className="movies-list">
-        <ul>
-          {sortedList.map((item, idx) => {
-            return (
-              <li key={`${idx}-${item}`}>
-                <p>Name: {item.name}</p>
-                <p>Release: {item.release}</p>
-                <p>Author: {item.author}</p>
-                <p>Type: {item.type}</p>
-                <button
-                  style={{ marginBottom: 5 }}
-                  onClick={(e) => handleDeleteMovie(e, item)}
-                >
-                  Delete
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      <MoviesListComponent
+        filterByRelease={filterByRelease}
+        globalSearchList={globalSearchList}
+        moviesList={moviesList}
+        setMoviesList={setMoviesList}
+        sortedKey={sortedKey}
+        sortedOrderAscending={sortedOrderAscending}
+      />
     </div>
   );
 }
